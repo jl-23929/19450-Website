1774515307d1ad6bd1a39569249f77164e0490f5
jl-23929 <james@lollback.com>
2024-11-28 23:06:58 +1000

    Made some layout changes and updated Our Robots page.

--- scripts.js
+++ scripts.js
@@ -29,13 +29,12 @@ import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/js
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
-const aspect = window.innerWidth / window.innerHeight;
 
 // Set up the frustum extents for the orthographic camera
 const frustumSize = 0.8; // Controls the size of the view area
 const camera = new THREE.OrthographicCamera(
-  (-frustumSize * aspect) / 2, // Left
-  (frustumSize * aspect) / 2, // Right
+  -frustumSize / 2, // Left
+  frustumSize / 2, // Right
   frustumSize / 2, // Top
   -frustumSize / 2, // Bottom
   0.1, // Near clipping plane
@@ -54,7 +53,7 @@ const loader = new GLTFLoader();
 loader.load(
   //TODO:
   //https://19450-website.s3.ap-southeast-2.amazonaws.com/Robot.glb
-  "Images/Robot Assembly.gltf",
+  "Images/Robot.glb",
   function (gltf) {
     object = gltf.scene;
     scene.add(object);
@@ -67,11 +66,10 @@ loader.load(
   }
 );
 
-const renderer = new THREE.WebGLRenderer({ alpha: false });
+const renderer = new THREE.WebGLRenderer({ alpha: true });
 
-camera.aspect = 10;
+camera.aspect = 100;
 camera.updateProjectionMatrix();
-
 renderer.setSize(innerHeight / 3, innerHeight / 3);
 camera.updateProjectionMatrix();
 renderer.render(scene, camera);
@@ -83,7 +81,7 @@ camera.lookAt(-1, -0.3, 0.2);
 //camera.lookAt(-1, 0, 0);
 const topLight = new THREE.DirectionalLight(0xffffff, 1);
 topLight.position.set(-1, -1, 1);
-topLight.castShadow = false;
+topLight.castShadow = true;
 scene.add(topLight);
 
 controls = new OrbitControls(camera, renderer.domElement);
@@ -100,7 +98,7 @@ function animate() {
 window.addEventListener("resize", function resizeRendererToSquare() {
   const size = Math.min(container.clientWidth, container.clientHeight); // Use the smallest dimension
   renderer.setSize(size, size); // Square dimensions
-  camera.aspect = 10; // Square aspect ratio
+  camera.aspect = 100; // Square aspect ratio
   camera.updateProjectionMatrix(); // Update the camera
 });
 
